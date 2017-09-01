cac65b2e46bb14a866ca091a7481ed1d1817bcf3
Fernando Soares <sax.soares@gmail.com>
2017-09-01 20:59:52 -0300

    Campo Desenhado

--- Ex1_Pong.c
+++ Ex1_Pong.c
@@ -4,34 +4,53 @@
 void desenhaCampo(void);
 void desenhaPlayers(void);
 
-GLfloat w_width;
-GLfloat w_height;
-GLfloat s_width;
-GLfloat s_height;
+GLfloat w_width, w_height;
+GLfloat s_width, s_height;
+GLfloat preto[] = {0,0,0},
+        branco[] = {1,1,1},
+        verde[] = {0,1,0};
+GLfloat cantoIEX = -0.95,cantoIEY = -0.95,cantoSDX = 0.95,cantoSDY = 0.95,
+        cantoIDX = 0.95, cantoIDY = -0.95,cantoSEX = -0.95,cantoSEY = 0.95;
+GLfloat x1TracejadoEsq = -0.80, x2TracejadoEsq = -0.80;
+GLfloat x1TracejadoDir = 0.80, x2TracejadoDir = 0.80;
 
 void desenhaCampo(void){
     w_width = glutGet(GLUT_WINDOW_WIDTH);
     w_height = glutGet(GLUT_WINDOW_HEIGHT);
+    s_width = glutGet(GLUT_SCREEN_WIDTH);
+    s_height = glutGet(GLUT_SCREEN_HEIGHT);
 
     glPolygonMode(GL_FRONT,GL_LINE);
-    glColor3f(0.0,1.0,0.0); glRectf(-0.95,-0.95,0.95,0.95);
-    glLineStipple(1, 1);
-
+    glColor3fv(branco); glRectf(cantoIEX,cantoIEY,cantoSDX,cantoSDY);
+    glBegin(GL_LINES);
+        glVertex2f(0, cantoSEY);
+        glVertex2f(0, cantoIDY);
+    glEnd();
+    glEnable(GL_LINE_STIPPLE);
+    glLineStipple(3, 0x00FF);
+    glBegin(GL_LINES);
+        glVertex2f(x1TracejadoEsq, cantoSEY);
+        glVertex2f(x2TracejadoEsq, cantoIDY);
+        glVertex2f(x1TracejadoDir, cantoSEY);
+        glVertex2f(x2TracejadoDir, cantoIDY);
+    glEnd();
+    glDisable(GL_LINE_STIPPLE);
 }
 void desenhaPlayers(void){
 
 }
-void Ex1_Pong(){
-    s_width = glutGet(GLUT_SCREEN_WIDTH);
-    s_height = glutGet(GLUT_SCREEN_HEIGHT);
 
+void bola(void){
+
+}
+
+void Ex1_Pong(){
     glClearColor(0.0,0.0,0.0,0);
     glClear(GL_COLOR_BUFFER_BIT);
     glShadeModel(GL_FLAT);
     glPointSize(1.0);
 
         desenhaCampo();
-        glutReshapeFunc(Ex1_Pong);
 
     glFlush();
     glFinish();
--- main.c
+++ main.c
@@ -3,11 +3,15 @@
 #include <GL/glut.h>
 #include "Ex1_Pong.h"
 
+GLint sw, sh, winPosX, winPosY;
+
 int main(int argc, char *argv[]){
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowPosition(0, 0);
-    glutInitWindowSize(glutGet(GLUT_SCREEN_WIDTH), glutGet(GLUT_SCREEN_HEIGHT));
+    sw = glutGet(GLUT_SCREEN_WIDTH), sh = glutGet(GLUT_SCREEN_HEIGHT),
+        winPosX = 0, winPosY = 0;
+    glutInitWindowPosition(winPosX, winPosY);
+    glutInitWindowSize(sw, sh);
     glutCreateWindow("CG Fernando Ex1-Pong");
 
     glutDisplayFunc(Ex1_Pong);
